7ec0e5d22b0b1334a6b24b81fbcb0aff06113b85
Kseniia <ksusha.7_7_7@mal.ru>
2021-03-31 23:08:52 +0300

    5.1 DI внедрение зависимости с помощью setter

--- src/main/java/ru/kseniia/springcourse/MusicPlayer.java
+++ src/main/java/ru/kseniia/springcourse/MusicPlayer.java
@@ -4,7 +4,12 @@ public class MusicPlayer {
     private Music music; // в качестве зависимости интерфейс
 
     // IoC
-    public MusicPlayer(Music music) { //передаем зависимости
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+    public MusicPlayer(){} // созд конструктор без аргументов
+
+    public void setMusic(Music music){ // созд сеттер. spring видит не setMusic а music
         this.music = music;
     }
     // получим песню
